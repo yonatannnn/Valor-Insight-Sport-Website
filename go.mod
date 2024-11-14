ca5b8f305001b3376dd79a21644f640b58c2646a
yonathan <yonatanalebachew7@gmail.com>
2024-11-14 23:34:00 +0300

    cors added to vite

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	github.com/gin-contrib/cors v1.7.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.10.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
@@ -27,6 +28,7 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/montanaflynn/stats v0.7.1 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/rs/cors v1.11.1 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
--- router/router.go
+++ router/router.go
@@ -1,14 +1,26 @@
 package router
 
 import (
+	"time"
 	"valorInsight/controllers"
 	"valorInsight/infrastructure"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter(tc *controllers.Controller) *gin.Engine {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"}, // Correct field for specifying allowed origins
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Content-Type", "Authorization", "Origin", "Accept"},
+		ExposeHeaders:    []string{"Content-Length"}, // Optional, exposes headers to the frontend
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
 	r.POST("/register", tc.RegisterUser)
 	r.POST("/login", tc.Login)
 	r.POST("/refresh", tc.RefreshToken)
